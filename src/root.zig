3184d1eb003df7286b302ecca5b26bbad53ad484
Lune4696 <lune.sqnm@gmail.com>
2025-05-13 10:46:21 +0900

    add library documentation

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,12 @@
+//! Zig BLAS and LAPACK based (dense) array library.
+//! zbla contains some functions
+//!     > core: basic array function
+//!         > array: column-order/major (dense) array Dense(T, n) and its method
+//!         > math: mathematical operations for Dense(T, n)
+//!     > graph: graph-related operations (graph laplacian, ...)
+//!     > solver: (non-)iterative linear algebra solver
+//!         > iterative: iterative solver (CG, MINRES, ...)
+//!
 const std = @import("std");
 
 pub const core = @import("./core/bind.zig");
